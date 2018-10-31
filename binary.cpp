067c3f569807efb92703fad3b2aa968b991a93bb
jorgemayna <jorgemayna@gmail.com>
2018-10-30 23:39:57 -0500

    Acabado gg wp

--- binary.cpp
+++ binary.cpp
@@ -5,30 +5,48 @@ class boolean {
     private:
         char* array;
         int size;
-    
+
     public:
-        boolean(int size) {
-            // TODO
+        boolean(int size):size(size) {
+            if(size%8==0){
+                array= new char[size/8];
+                for(int i=0;i<size/8;i++){
+                    array[i]=0;
+                }
+            }
+            else{
+                array= new char[size/8+1];
+                for(int i=0;i<size/8+1;i++){
+                    array[i]=0;
+                }
+            }
         }
-        
+
         void bitOn(int position) {
-            // TODO
+            array[position/8] |=1<<(position%8);
         }
-             
+
         void bitOff(int position) {
-            // TODO
+            array[position/8] &=~(1<<(position%8));
         }
-             
+
         bool operator[](int position) {
-            // TODO
+            if((array[position/8] & 1<<(position%8))==0){
+                return 0;
+            }else{
+                return 1;
+            }
+
         }
-    
-        void clear() {    
-            // TODO
+
+        void clear() {
+            for(int i=0;i<size;i++){
+                array[i]=0;
+            }
         }
-             
+
         ~boolean() {
-            // TODO
+            delete array;
         }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
-
+#include "binary.cpp"
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    boolean bol(8);
     cout << "===========================================================" << endl;
     cout << "\Binary Array Practice" << endl;
     cout << "===========================================================" << endl << endl;
+    for(int i=0;i<8;i++){
+        bol.bitOn(i);
+    }
+    bol.bitOff(4);
+    for(int i=7;i>=0;i--){
+        cout<<bol[i];
+    }
+    bol.clear();
+    cout<<endl;
+    for(int i=7;i>=0;i--){
+        cout<<bol[i];
+    }
+    bol.~boolean();
 
-    system("read");
-    return EXIT_SUCCESS;
+    return 0;
 }
 
